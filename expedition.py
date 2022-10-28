a84be81a5f076704c19272e315fbb3c2ba47778a
MomoBD <momoblydblk@gmail.com>
2022-10-28 16:11:41 -0400

    Init Commit. Expedition Class init function done

---
+++ expedition.py
@@ -0,0 +1,15 @@
+from timer import Timer
+
+
+class Expedition:
+
+    expeditionID = 0          # Expedition ID, int, 1-41
+    expeditionTime = 0        # Expedition Time, int, unit:seconds
+    bindTimer = None
+    fleet=0
+
+    def __init__(self, eid: int, time: int, fleet: int, timerInstance: Timer):
+        self.expeditionID = eid
+        self.expeditionTime = time
+        self.fleet = fleet
+        self.bindTimer = timerInstance
